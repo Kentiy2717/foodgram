d2fcc6854eb5899539f9c3aa004eb0d6808a46a8
Innokentiy Motriy <79234101338@yandex.ru>
2024-08-08 13:07:24 +0700

    Delete 'v1/' in urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,15 +5,16 @@ from .views import SignUpView, UsersViewSet
 
 v1_router = DefaultRouter()
 v1_router.register('users', UsersViewSet, basename='users')
+v1_router.register('signup', UsersViewSet, basename='users')
 
 v1_auth_urls = [
-    path('', include('djoser.urls')),  # ???
-    path('', include('djoser.urls.jwt')),  # ???
+#     path('', include('djoser.urls')),  # ???
+#     path('', include('djoser.urls.jwt')),  # ???
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
 
 
 urlpatterns = [
-    path('v1/', include(v1_router.urls)),
-    path('v1/auth/', include(v1_auth_urls)),
+    path('', include(v1_router.urls)),
+    path('auth/', include(v1_auth_urls)),
 ]
