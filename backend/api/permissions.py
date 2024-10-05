963a8bf87795799ee2e3d93ad3948bc98dd5e976
Innokentiy Motriy <79234101338@yandex.ru>
2024-10-06 00:39:27 +0700

    ver. 1.3

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,14 +2,14 @@ from rest_framework import permissions
 
 
 class IsAuthenticatedOwnerOrReadOnly(permissions.IsAuthenticated):
-    def has_permission(self, request):
+    def has_permission(self, request, view):
         return bool(
             (request.method in permissions.SAFE_METHODS)
             or request.user and request.user.is_authenticated
         )
 
-    def has_object_permission(self, request, obj):
+    def has_object_permission(self, request, view, obj):
         return bool(
-            request.method in permissions.SAFE_METHODS
+            (request.method in permissions.SAFE_METHODS)
             or obj.author == request.user
         )
