bccfaf273c85d174702f51dc58ad9ebc6460aa91
Innokentiy Motriy <79234101338@yandex.ru>
2024-10-06 00:48:33 +0700

    ver. 1.6

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -10,5 +10,6 @@ class IsAuthenticatedOwnerOrReadOnly(permissions.IsAuthenticated):
 
     def has_object_permission(self, request, view, obj):
         return bool(
-            obj.author == request.user
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
         )
