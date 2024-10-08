60e81884c2f915df75d78fa48f7b7c66f2e0d2d9
Innokentiy Motriy <79234101338@yandex.ru>
2024-10-08 21:28:38 +0700

    ver. 1.9 без зелени

--- backend/food/urls.py
+++ backend/food/urls.py
@@ -1,12 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-# from food.views import GetLinkView
 from food.views import GetLinkViewSet
-
-# urlpatterns = [
-#     path('', GetLinkView.as_view(), name='get_link'),
-# ]
 
 v1_router = DefaultRouter()
 v1_router.register('s', GetLinkViewSet, basename='get_link')
